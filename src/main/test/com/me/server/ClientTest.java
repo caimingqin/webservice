ad0eefa2fee6dfedf1687c456e7a005ab0fc5e81
cai.mingqin <737895494@qq.com>
2013-03-19 17:44:39 +0800

    Change-Id: I23bc59a30f469561aee3cf7d4ff457da01a56d6e

--- src/main/test/com/me/server/ClientTest.java
+++ src/main/test/com/me/server/ClientTest.java
@@ -6,7 +6,7 @@ import java.rmi.RemoteException;
 public class ClientTest {
 
 	public static void main(String[] args) throws RemoteException {
-		//根据wsdl文件生成本地java客户端代码调�?
+		//根据wsdl文件生成本地java客户端代码,进行调用
 		HelloWorldStub helloWorldStub = new HelloWorldStub("http://localhost/axis2/services/HelloWorld");
 		GetResponse getResponse = helloWorldStub.get(new Get());
 		String get_return = getResponse.get_return();
